831f33713a97790d5e45ab91e314bf7552403f99
ThandarPhyoe <thandarthuphyoe@gmail.com>
2018-06-26 06:04:23 -0700

    ex9.py

---
+++ Python_exercises/ex9.py
@@ -0,0 +1,8 @@
+days="Mon Tue Wed Thu Fri Sat Sun"
+months="Jan\nFeb\nMar\nApr\nMay\nJune\nJuly\nAug"
+print("Here are the days:",days)
+print("Here are the months:",months)
+print("There's something going on here.")
+print("With the three doouble-quotes.")
+print("We'll be able to type as much as we like.")
+print("Even 4 lines if we want, or 5, or 6.")
