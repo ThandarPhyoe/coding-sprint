e571bc7402778b6fe022ca940ad7805c9f378ae2
ThandarPhyoe <thandarthuphyoe@gmail.com>
2018-06-26 22:34:22 -0700

    ex14.py

--- Python_exercises/ex14.py
+++ Python_exercises/ex14.py
@@ -1,16 +1,21 @@
-from sys import argv 
+from sys import argv
+
 script, user_name = argv
 prompt = '> '
-print(f"Hi {user_name}, I'm the {script} script.")
-print("I'd like to ask you a few questions .")
-print(f"Do you like me {user_name}?")
+
+print("Hi %s, I'm the %s script." % (user_name, script))
+print("I'd like to ask you a few questions.")
+print("Do you like me %s?" % user_name)
 likes = input(prompt)
-print(f"Where do you live {user_name}?")
+
+print("Where do you live %s?" % user_name)
 lives = input(prompt)
+
 print("What kind of computer do you have?")
 computer = input(prompt)
-print(f"""
-Alright, so you said {likes} about liking me.
-You live in {lives}. Not sure where that is.
-And you have a {computer} computer. Nice.
-""")
+
+print("""
+Alright, so you said %r about liking me.
+You live in %r. Not sure where that is.
+And you have a %r computer. Nice.
+""" % (likes, lives, computer))
